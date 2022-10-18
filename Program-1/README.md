48fb6f980045d022ec548b9520be10c4d781aca9
Anmol Anand <anmolanand2k15@gmail.com>
2022-10-18 13:53:51 +0530

    Added Program-1

---
+++ Program-1/README.md
@@ -0,0 +1,24 @@
+# ALGORITHM
+
+- Step-1 :- START
+- Step-2 :- Import `java.util.*` package.
+- Step-3 :- Create a class named as `prime_adam`.
+- Step-4 :- Create a function named as `isPrime` which takes an integer as an argument and returns a boolean value. It checks whether the number is prime or not.
+- Step-5 :- Create a function named as `reverse` which takes an integer as an argument and returns an integer. It reverses the number.
+- Step-6 :- Create a function named as `isAdam` which takes an integer as an argument and returns a boolean value. It checks whether the number is Adam or not.
+- Step-7 :- Create a function named as `main` to call the methods and print the result.
+- Step-8 :- END
+
+# VD TABLE 
+
+| Sr. No. | Variable | Data Type | Description |
+| --- | --- | --- | --- |
+| 1 | x | int | To store the number |
+| 2 | r | int | To store the reverse of the number |
+| 3 | s1 | int | To store the square of the number |
+| 4 | s2 | int | To store the square of the reverse of the number |
+| 5 | i | int | To store the value of the loop variable |
+| 6 | c | int | Used As Counter Variable  |
+| 7 | count | int | Used As Counter Variable |
+| 8 | m | int | To store the upper limit for the loop |
+| 9 | n | int | To store the lower limit for the loop |
---
+++ Program-1/prime_adam.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class prime_adam {
+    public static boolean isPrime(int x) {
+        int c=0;
+        for (int i=1;i<=x;i++) {
+            if (x%i==0) {
+                c++;
+            }
+        }
+        if (c==2) {
+            return true;
+        }
+        return false;
+    } 
+    public static int reverse(int n) {
+        int rev=0,d=0;
+        while (n!=0) {
+            d=n%10;
+            rev = rev*10+d;
+            n/=10;
+        }
+        return rev;
+    }
+    public static boolean isAdam(int n) {
+        int s1 = n*n;
+        int rev = reverse(n);
+        int s2 = rev*rev;
+        int r = reverse(s2);
+
+        return r==s1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Value Of m");
+        int m = sc.nextInt();
+        System.out.println("Enter The Value Of n");
+        int n = sc.nextInt();
+        
+        if (m>=n) {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        int count = 0;
+
+        System.out.println("Prime Adam Numbers Are - ");
+        for (int i=m;i<=n;i++) {
+            if (isAdam(i) && isPrime(i)) {
+                System.out.println(i+"\t");
+                count++;
+            }
+        }
+        System.out.println("Frequency - "+count);
+
+        if (count == 0) {
+            System.out.println("Nil");
+        }
+    }
+}
