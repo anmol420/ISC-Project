8984f17706ce7d96082e8da67833e8fa1ad34392
Anmol Anand <anmolanand2k15@gmail.com>
2022-10-20 21:41:51 +0530

    Added Program-5

---
+++ Program-5/README.md
@@ -0,0 +1,26 @@
+# ALGORITHM
+
+- Step-1 :- START
+- Step-2 :- Create a class named as `sort`.
+- Step-3 :- Create a method named as `sort` and pass the integer type array `a[]` as parameter. In this function, create a for loop (from 0 to the length of the array), start a inner loop (from 0 to `a.length-1-i`) and check whether `a[j] > a[j + 1]`, if true then swap them if they are not in the desired order. 
+- Step-4 :- Create a method named as `main`. In this function, create an integer type array `a[]` and store the elements in it (user input). Call the function `sort` and pass the array `a[]` as the parameter. Print the sorted array. Create a 2-Dimensional array `b[][]` and store the elements of sorted array `a[]` in the required pattern using for-loops. Now print the 2-Dimensional array `b[][]`.
+- Step-5 :- END
+
+# VD TABLE
+
+| Sr. No. | Variable | Data Type | Description |
+| --- | --- | --- | --- |
+| 1 | i | int | To store the value of the loop variable |
+| 2 | j | int | To store the value of the loop variable |
+| 3 | a | int[] | To store the elements of the array |
+| 4 | b | int[][] | To store the elements of the array in the required pattern |
+| 5 | n | int | To store the size of the array |
+| 6 | temp | int | To store the temporary value |
+| 7 | k | int | To store the value of the loop variable |
+| 8 | r | int | To store the value of the loop variable |
+
+# OUTPUT
+
+<p align="center">
+<img width="920" height="200" alt="output" src="output.png">
+</p>
---
+++ Program-5/sort.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class sort
+{
+    public static void sortArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+            } 
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER VALUE OF N: ");
+        int n = sc.nextInt();
+        
+        if (n <= 2 || n >= 10) {
+            System.out.println("MATRIX SIZE OUT OF RANGE");
+            return;
+        }
+        
+        int a[] = new int[n];
+        int b[][] = new int[n][n];
+        
+        System.out.println("ENTER ELEMENTS OF SINGLE DIMENSIONAL ARRAY:");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        sortArray(a);
+        System.out.println("SORTED ARRAY:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        
+        for (int i = n - 1, r = 0; i >= 0; i--, r++) {
+            for (int j = 0; j <= i; j++) {
+                b[r][j] = a[j];
+            }
+            
+            for (int k = n - 1; k > i; k--) {
+                b[r][k] = a[k - i - 1];
+            }
+        }
+        
+        System.out.println();
+        System.out.println("FILLED MATRIX:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
