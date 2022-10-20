3c38ba2a21c8bdffb806ba14678b767bedebb57b
Anmol Anand <anmolanand2k15@gmail.com>
2022-10-20 21:23:48 +0530

    Added Program-4

---
+++ Program-4/README.md
@@ -0,0 +1,26 @@
+# ALGORITHM
+
+- Step-1 :- START
+- Step-2 :- Create a class named as `days`.
+- Step-3 :- Create a method named as `main` and declare the Scanner class. Declare the variables- `n`,`day`,`year`- of integer type to store the number of days, calculated days and year respectively. Create an array named `a` of integer type in order to store the names of the 12 months orderwise. Check whether the year entered is a leap year or not, if true then store the number of days in the month of February as 29 else store it as 28. Using multiple for-loops store the calculated days in the variable `day` and the year in the variable `year`. Finally print the calculated date,month and y
+- Step-4 :- END
+
+# VD TABLE
+
+| Sr. No. | Variable | Data Type | Description |
+| --- | --- | --- | --- |
+| 1 | n | int | Stores the number of days |
+| 2 | day | int | Stores the days |
+| 3 | year | int | Stores the year |
+| 4 | a | int[] | Stores the names of the 12 months orderwise |
+| 5 | i | int | Used in for-loop |
+| 6 | j | int | Used in for-loop |
+| 7 | k | int | Used in for-loop |
+| 8 | nday | int | Stored the calculated day number |
+| 9 | date | int | Stores the calculated date |
+
+# OUTPUT
+
+<p align="center">
+<img width="920" height="200" alt="output" src="output.png">
+</p>
---
+++ Program-4/days.java
@@ -0,0 +1,78 @@
+import java.util.*;
+public class days {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n, day, year, i, p, k=0;
+        int ar[]={0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+        String month[]={"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        System.out.print("DAY NUMBER: ");
+        day = sc.nextInt();
+        System.out.print("YEAR: ");
+        year =sc.nextInt();
+        System.out.print("DATE AFTER (N DAYS): ");
+        n = sc.nextInt();
+        p = day + n;
+        int a = 1, b = 1, date = 0, nday = 0, year1 = year;
+        if(year % 4 == 0)
+        {
+            for(i = 2; i <= 12; i++)
+            {
+                ar[i] += 1;
+            }
+        }
+        if(day >= 1 && day <= ar[12])
+        {
+            for(i = 0; i <= 12; i++)
+            {
+                if(ar[i] < day)
+                {
+                    a = i;
+                }
+                if(ar[i] > day)
+                {
+                        break;
+                }
+            }
+            date = day - ar[a];
+        }
+        else
+        {
+            k = 1;
+        }
+        if(k == 1)
+        {
+            System.out.println("Days are out of Range");
+        }
+        else if(k != 1 && n >= 1 && n <= 100)
+        {
+            if(p > ar[12])
+            {
+                p = p - ar[12];
+                year1 = year1 + 1;
+            }
+            for(i = 0; i <= 12; i++)
+            {
+                if(ar[i] < p)
+                {
+                    b = i;
+                }
+                if(ar[i] > p)
+                {
+                    break;
+                }
+            }
+            nday = p - ar[b];
+        }
+        else
+        {
+            k = 1;
+            System.out.println("N Days are out of range ");
+        }
+        if(k != 1)
+        {
+            System.out.println("DATE : " + date + "th " + month[a + 1] + " " + year);
+            System.out.println("DATE AFTER "+n+" DAYS: "+ nday + "th " + month[b + 1] + " " + year1);
+        }
+    }
+}
