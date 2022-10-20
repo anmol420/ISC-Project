a9ef75d92fcbf28728963be1330a39cbdea1dfa9
Anmol Anand <anmolanand2k15@gmail.com>
2022-10-20 22:13:04 +0530

    Added Program-6

---
+++ Program-6/README.md
@@ -0,0 +1,30 @@
+# ALGORITHM
+
+- Step-1 :- START
+- Step-2 :- Create a class named as `pallin`.
+- Step-3 :- Create a method named as `isPallindrome` and pass a string named `word` as a parameter. In this function check whether the string is pallindrome or not.
+- Step-4 :- Create a method named as `makePalindrome` and pass a string named `word` as a parameter. In this function, declare variables to store length and the last character of the string `word` in `len` and `lastChar` respectively. Using a StringBuffer class and a for-loop, append the string `word` to the StringBuffer object `sb` and then append the last character of the string `word` to the StringBuffer object `sb`. Then, return the StringBuffer object `sb` as a string.
+- Step-5 :- Create a method named as `main`. In this function, using Scanner class take the input of the string `Str` as input. Then, call the method `isPallindrome` and pass the string `word` as a parameter. If the method returns `true`, then print the string `Str`. Then, call the method `makePalindrome` and pass the string `word` as a parameter. Then, print the string returned by the method `makePalindrome`.
+- Step-6 :- END
+
+# VD TABLE
+
+| Sr. No. | Variable | Data Type | Description |
+| --- | --- | --- | --- |
+| 1 | Str | String | To store the input string |
+| 2 | word | String | To store the words of the string |
+| 3 | str | String | To store the string without front and back spaces |
+| 4 | len | int | To store the length of the string |
+| 5 | lastChar | char | To store the last character of the string |
+| 6 | i | int | Used in for-loop |
+| 7 | isPalindrome | boolean | To check whether the word is pallindrome or not |
+| 8 | palin | boolean | To check whether the word is pallindrome or not |
+| 9 | palinWord | String | To store the string returned by the method `makePalindrome` |
+| 10 | convertedStr | String | To store the converted string |
+
+# OUTPUT
+
+<p align="center">
+<img width="920" height="200" alt="output" src="output.png">
+</p>
+
---
+++ Program-6/pallin.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class pallin {
+    public static boolean isPalindrome(String word) {
+        boolean palin = true;
+        int len = word.length();
+        for (int i = 0; i <= len / 2; i++) {
+            if (word.charAt(i) != word.charAt(len - 1 - i)) {
+                palin = false;
+                break;
+            }
+        }
+        return palin;
+    }
+    public static String makePalindrome(String word) {
+        int len = word.length();
+        char lastChar = word.charAt(len - 1);
+        int i = len - 1;
+        while (word.charAt(i) == lastChar) {
+            i--;
+        }
+        StringBuffer sb = new StringBuffer(word);
+        for (int j = i; j >= 0; j--) {
+            sb.append(word.charAt(j));
+        }
+        
+        return sb.toString();
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE SENTENCE:");
+        String Str = sc.nextLine().trim().toUpperCase();
+        int len = Str.length();
+        
+        char lastChar = Str.charAt(len - 1);
+        if (lastChar != '.' 
+            && lastChar != '?' 
+            && lastChar != '!') {
+                System.out.println("INVALID INPUT");
+                return;
+        }
+        
+        String str = Str.substring(0, len - 1);
+        
+        StringTokenizer st = new StringTokenizer(str);
+        StringBuffer sb = new StringBuffer();
+        
+        while (st.hasMoreTokens()) {
+            String word = st.nextToken();
+            boolean isPalinWord = isPalindrome(word);
+            if (isPalinWord) {
+                sb.append(word);
+            }
+            else {
+                String palinWord = makePalindrome(word);
+                sb.append(palinWord);
+            }
+            sb.append(" ");
+        }
+
+        String convertedStr = sb.toString().trim();
+        
+        System.out.println();
+        System.out.println(Str);
+        System.out.println(convertedStr);
+    }
+}
