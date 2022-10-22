7e2e17f123de19e900e4a03e58cfe9cb1d576d97
Anmol Anand <anmolanand2k15@gmail.com>
2022-10-22 13:15:35 +0530

    Added Program-10

---
+++ Program-10/README.md
@@ -0,0 +1,23 @@
+# ALGORITHM
+
+- Step-1 :- START
+- Step-2 :- Create a class named as `boxes`.
+- Step-3 :- Create a method named as `main`. In this function, create a variable named as `n` to store the number of boxes. Check if the number of boxes is greater than 0 and less than 1000 or not, if true then print `Invalid Input` and exit the program. Create an array named as `cartonSizes` to store the various sizes of the boxes. Create a for-loop (from 0 to length of `cartonSizes`) inside which calculate and print the volume of each box size. Print the total volume of all the boxes.
+- Step-4 :- END
+
+# VD TABLE
+
+| Sr. No. | Variable | Data Type | Description |
+| --- | --- | --- | --- |
+| 1 | n | int | To store the number of boxes |
+| 2 | cartonSizes | int[] | To store the various sizes of the boxes |
+| 3 | i | int | To store the index of the array |
+| 4 | cartonCount | int | To store the volume of each box size |
+| 5 | total | int | To store the total number of all the boxes |
+| 6 | t | int | Used as temporary variable |
+
+# OUTPUT
+
+<p align="center">
+<img width="920" height="200" alt="output" src="output.png">
+</p>
---
+++ Program-10/boxes.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class boxes {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of boxes (N): ");
+        int n = sc.nextInt();
+        
+        if (n < 1 || n > 1000) {
+            System.out.println("INVALID INPUT");
+            return;
+        }
+
+        int cartonSizes[] = {48, 24, 12, 6};
+
+        int total = 0;
+        int t = n;
+        for (int i = 0; i < cartonSizes.length; i++) {
+            int cartonCount = t / cartonSizes[i];
+            t = t % cartonSizes[i];
+            total += cartonCount;
+            if (cartonCount != 0) {
+                System.out.println(cartonSizes[i] + " * " + cartonCount + 
+                    " = " + (cartonSizes[i] * cartonCount));
+            }
+        }
+
+        if (t != 0) {
+            System.out.println("Remaining boxes = " + t 
+                + " * 1 = " + t);
+            total++;
+        }
+        else {
+            System.out.println("Remaining boxes = 0");
+        }
+
+        System.out.println("Total number of boxes = " + n);
+        System.out.println("Total number of cartons = " + total);
+    }
+}
